2a5093d544762b724323e06759f7925b7b1b1710
Guilherme S. Duarte <116381036+GuilhermeDSD19@users.noreply.github.com>
2022-11-14 18:11:36 -0300

    Estrutura Condicional IF

---
+++ javascript/cursoJS-Guanabara/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 75
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permetida. MULTADO!`)
+}
+console.log(`Diriga sempre usando cinto de segurança`)
---
+++ javascript/cursoJS-Guanabara/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Você nasceu no ${pais}.`)
+if (pais == 'Brasil'){
+    console.log('Então, Você é Brasileiro!')
+} else {
+    console.log('Então, você é Estrangeiro!')
+}
