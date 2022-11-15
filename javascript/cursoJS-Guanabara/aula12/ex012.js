0414efb4e7f7d894aa7145a523be9d4664b3a4a1
Guilherme S. Duarte <116381036+GuilhermeDSD19@users.noreply.github.com>
2022-11-14 21:53:39 -0300

    Estrutura de Decisão Switch

---
+++ javascript/cursoJS-Guanabara/aula12/ex012.js
@@ -0,0 +1,11 @@
+var agora = new Date()
+var hora = agora.getHours();
+
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log("Bom Dia!")
+} else if (hora <= 18) {
+    console.log("Boa Tarde!")
+} else {
+    console.log("Bom Noite!")
+}
