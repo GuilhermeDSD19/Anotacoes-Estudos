babd9515ec2dbdf3ca3bebe5ef034308cd41f2f7
Guilherme S. Duarte <116381036+GuilhermeDSD19@users.noreply.github.com>
2022-11-19 15:13:11 -0300

    Funções com Parâmetros

---
+++ javascript/cursoJS-Guanabara/aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+let res = parimpar(8)
+console.log(res)
+
+console.log(parimpar(8))
+
---
+++ javascript/cursoJS-Guanabara/aula16/funcao05.js
@@ -0,0 +1,11 @@
+// RECURSIVIDADE
+
+function fatorial(n){
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorial(170))
