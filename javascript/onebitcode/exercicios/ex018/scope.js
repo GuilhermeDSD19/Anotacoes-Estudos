ebcb06e9fe55fb00c756aac5712983e80999c0f1
Guilherme S. Duarte <116381036+GuilhermeDSD19@users.noreply.github.com>
2022-12-16 17:49:40 -0300

    Scope

---
+++ javascript/onebitcode/exercicios/ex018/scope.js
@@ -0,0 +1,10 @@
+function setSpaceshipDetails() {
+    console.log(spaceshipName)
+    console.log(spaceshipType)
+    var spaceshipName = "Elemental"
+    let spaceshipType = "Discovery"
+    console.log(spaceshipName)
+    console.log(spaceshipType)
+}
+
+setSpaceshipDetails()
