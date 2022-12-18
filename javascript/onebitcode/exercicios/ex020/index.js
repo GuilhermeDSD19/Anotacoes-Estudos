9f512f963508726591a1bfa9f9526ccfac97b247
Guilherme S. Duarte <116381036+GuilhermeDSD19@users.noreply.github.com>
2022-12-18 19:08:16 -0300

    Métodos splice e slice

--- javascript/onebitcode/exercicios/ex020/index.js
+++ javascript/onebitcode/exercicios/ex020/index.js
@@ -26,8 +26,6 @@ indiceMilan = testeArray.indexOf("Palmeiras")
 
 console.log(testeArray, indiceMilan)
 
-*/
-
 let arrayBidimensinal = [ ["Elemental", 7], ["Helmet", 13], ["Golias", 9], ["Supernova", 9] ]
 
 console.log(arrayBidimensinal[3][0])
@@ -35,3 +33,21 @@ console.log(arrayBidimensinal[3][0])
 let arrayBidimensinal2 = [ ["Elemental", 7], ["Helmet", 13], ["Golias", 9], ["Supernova", "9", [true, false]] ]
 
 console.log(arrayBidimensinal2[3][2][1])
+
+let spaceshipNames = ["Elemental", "Darwin", "Artemis", "Supernova"]
+
+console.log(spaceshipNames)
+
+let removedSpaceships = spaceshipNames.splice(1, 2 , "teste")
+
+console.log(spaceshipNames + '\n' + removedSpaceships)
+
+*/
+
+let spaceshipNames = ["Elemental", "Darwin", "Artemis", "Supernova"]
+
+console.log(spaceshipNames)
+
+let extractedNames = spaceshipNames.slice(1,3)
+
+console.log(spaceshipNames + "\n" + extractedNames)
