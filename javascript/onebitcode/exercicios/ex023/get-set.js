65441fcc270fed0858252876555e69f943ed489f
guilhermedsd19 <guilhermesilva19860@gmail.com>
2022-12-26 16:41:21 -0300

    Método GET

--- javascript/onebitcode/exercicios/ex023/get-set.js
+++ javascript/onebitcode/exercicios/ex023/get-set.js
@@ -1,11 +1,11 @@
-class TransportSpaceship{
-    constructor (name){
+class TransportSpaceship {
+    constructor(name) {
         this.name = name
         this.currentVelocity = 0
     }
 
-    set velocity (newVelocity){
-        if (newVelocity > 120){
+    set velocity(newVelocity) {
+        if (newVelocity > 120) {
             this.currentVelocity = 120
         } else {
             this.currentVelocity = newVelocity
@@ -18,3 +18,24 @@ var spaceship = new TransportSpaceship("Transportador")
 spaceship.velocity = 145
 
 console.log(spaceship)
+
+
+
+class ResourceProcessStation {
+    constructor(name, monthlyProcessedLoad) {
+        this.name = name
+        this.monthlyProcessedLoad = monthlyProcessedLoad
+    }
+
+    get weeklyProcessedLoad() {
+        return this.monthlyProcessedLoad / 4
+    }
+}
+
+let ResourceProcesor = new ResourceProcessStation("Gaia", 500)
+
+console.log(ResourceProcesor.weeklyProcessedLoad)
+
+ResourceProcesor.monthlyProcessedLoad = 800
+
+console.log(ResourceProcesor.weeklyProcessedLoad)
