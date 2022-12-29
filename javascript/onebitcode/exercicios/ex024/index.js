38ad3b8793cfac8a9802443a6bfec70d266106d7
guilhermedsd19 <guilhermesilva19860@gmail.com>
2022-12-29 16:44:20 -0300

    Segundo menu (Desafio)

--- javascript/onebitcode/exercicios/ex024/index.js
+++ javascript/onebitcode/exercicios/ex024/index.js
@@ -1,4 +1,3 @@
-
 import {
     Naves
 } from "./class.js"
@@ -10,8 +9,7 @@ var type
 var qtyArmas
 var qtyAssentos
 
-// Cadastro Inicial
-function cadastroDaNave() { 
+function cadastroDaNave() { // Cadastro Inicial
     nomeDaNave = prompt("Digite o nome da nave")
     qtyTripulantes = prompt("Quantidade de tripulantes da nave")
     do {
@@ -28,13 +26,47 @@ function cadastroDaNave() {
     } while (type != "Batalha" && type != "Transporte")
 }
 
-// Instanciação 
+// Segundo Menu
+var escolhaDoMenu
+
+function showMenu() {
+    do { // Filtro de resposta
+        escolhaDoMenu = prompt("Escolha uma das opções abaixo: \n\n1- Acelerar a nave \n2- Trocar de nave \n3- Imprimir e sair")
+    } while (escolhaDoMenu != "1" && escolhaDoMenu != "2" && escolhaDoMenu != "3")
+}
+
+function acaoDoMenu() {
+    do { // Funções do Menu
+        switch (escolhaDoMenu) {
+            case "1": // Acelerar a Nave
+                console.log("TESTE")
+                showMenu()
+                break
+            case "2": // Trocar de nave
+                showMenu()
+                break
+            case "3": // Imprimir e Encerrar 
+
+                break
+            default:
+                showMenu()
+                break
+        }
+    } while (escolhaDoMenu != "3")
+}
+
+
+
+
+//                  PROGRAMA PRINCIPAL 
+
+// Instanciação da Classe
 var armasOuAssentos = cadastroDaNave()
 var nomeDaNave = new Naves(nomeDaNave, qtyTripulantes, type)
 nomeDaNave.assentoOuArmas(armasOuAssentos)
-console.log(nomeDaNave)
 
-
-
+// Segundo Menu
+showMenu()
+acaoDoMenu()
 
 
