39a03036a3e4c8b4d169600db615ed2173818c00
guilhermedsd19 <guilhermesilva19860@gmail.com>
2023-01-05 17:54:29 -0300

    Revisão das especificidades dos tipos de variaveis, Math e Arrays

---
+++ javascript/Web Moderno com JavaScript/exercicios-js/01-fundamentos/math.js
@@ -0,0 +1,10 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+const teste = Math.pow(raio, 4)
+
+console.log(area)
+console.log(raio)
+console.log(teste)
+
+var n = new Date
+console.log(n)
