0eab57eb64c879ba086a7fe1f6829b0e24956192
guilhermedsd19 <guilhermesilva19860@gmail.com>
2023-01-16 16:51:05 -0300

    this e bind

---
+++ javascript/Web Moderno com JavaScript/exercicios-js/04-funcoes/this-e-bind.js
@@ -0,0 +1,17 @@
+const pessoa = {
+    saudacao: 'Bom dia!', 
+    falar() {
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+const falar = pessoa.falar
+falar() // Conflito entre paradigmas: funcional e OO
+
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()
+
+const falar2 = pessoa.falar 
+falar2()
+
