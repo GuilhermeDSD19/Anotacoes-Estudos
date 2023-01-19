d4e25dedad3a9efdbca611883dfdb703313c8719
guilhermedsd19 <guilhermesilva19860@gmail.com>
2023-01-19 16:43:32 -0300

    Função construtora

---
+++ javascript/Web Moderno com JavaScript/exercicios-js/04-funcoes/funcaoConstrutora.js
@@ -0,0 +1,29 @@
+function Carro (velocidadeMaxima = 200, delta = 5) {
+    // Metodo Privado
+    let velocidadeAtual = 0
+
+    // Metodo Publico
+    this.acelerar = function () { 
+        if (velocidadeAtual + delta <= velocidadeMaxima) {
+            velocidadeAtual += delta
+        } else {
+            velocidadeAtual = velocidadeMaxima
+        }
+    }
+
+    // Metodo Publico
+    this.getVelocidadeAtual = function () {
+        return velocidadeAtual
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
+
+console.log(typeof Carro)
+console.log(typeof ferrari)
