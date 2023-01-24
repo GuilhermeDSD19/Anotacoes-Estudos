6a08ba2599134d09e07bada3dee15c4df990c75b
guilhermedsd19 <guilhermesilva19860@gmail.com>
2023-01-24 18:12:23 -0300

    Herança

---
+++ javascript/Web Moderno com JavaScript/exercicios-js/objeto/herança1.js
@@ -0,0 +1,18 @@
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324
+}
+
+const volvo = {
+    modelo: 'V40',
+    velMax: 200
+}
+
+console.log(ferrari.__proto__)
+console.log(ferrari.__proto__ === Object.prototype)
+console.log(volvo.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__)
+
+function MeuObjeto(){}
+console.log(typeof Object, typeof MeuObjeto)
+console.log(Object.prototype, MeuObjeto.prototype)
