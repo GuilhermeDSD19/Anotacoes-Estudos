a1945f2d48cdfe5bf933cc95814bbc0e2eee615c
guilhermedsd19 <guilhermesilva19860@gmail.com>
2023-01-27 19:59:10 -0300

    JSON to Object

---
+++ javascript/Web Moderno com JavaScript/exercicios-js/objeto/JSON-OBJ.js
@@ -0,0 +1,5 @@
+const obj = { a: 1, b: 2, c: 3, soma() { return a + b + c } }
+console.log(JSON.stringify(obj))
+
+console.log(JSON.parse('{"a": 1, "b": 2, "c": 3 }'))
+console.log(JSON.parse('{ "a": 1, "b": "string", "c": true, "d": {}, "e": [] }'))
