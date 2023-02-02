13994d9fa7508d999ae871aa7a971af17c086a23
guilhermedsd19 <guilhermesilva19860@gmail.com>
2023-02-02 18:11:04 -0300

    Resolução de exercicios

---
+++ javascript/Web Moderno com JavaScript/JS/00-exercicios/04.js
@@ -0,0 +1,5 @@
+function divisao(x, y) {
+    console.log(`${x} / ${y} = ${(x/y).toFixed(4)} \nResto de divisão: ${x % y}`)
+}
+
+divisao(10,3)
---
+++ javascript/Web Moderno com JavaScript/JS/00-exercicios/gabarito/fundamentos08.js
@@ -0,0 +1,25 @@
+let pontuacoes = [30, 40, 20, 4, 51, 25, 42, 38, 56, 0]
+
+function avaliaPontuacoes (pontuacoes) {
+    let qtdQuebraDeRecords = 0
+    let piorJogo = 1
+    let maiorPontuacao = pontuacoes[0]
+    let menorPontuacao = pontuacoes[0]
+    
+    for (let i = 1; i < pontuacoes.length; i++) {
+        if(pontuacoes[i] > maiorPontuacao) {
+            maiorPontuacao = pontuacoes[i]
+            qtdQuebraDeRecords++
+        }else if (pontuacoes[i] < menorPontuacao) {
+            menorPontuacao = pontuacoes[i]
+            piorJogo = i+1;
+        }
+    }
+    return [qtdQuebraDeRecords, piorJogo]
+}
+
+console.log(avaliaPontuacoes(pontuacoes))
+
+
+
+
