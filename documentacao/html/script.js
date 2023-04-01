a36812e2fc7dcf4ebdbbce2a80ed2d2813882118
GuilhermeDSD19 <guilhermesilva19860@gmail.com>
2023-04-01 15:13:19 -0300

    novalidate, :in-range, :out-of-range

---
+++ documentacao/html/script.js
@@ -0,0 +1,45 @@
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const error = email.nextElementSibling;
+
+// As per the HTML Specification
+const emailRegExp =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+// Now we can rebuild our validation constraint
+// Because we do not rely on CSS pseudo-class, we have to
+// explicitly set the valid/invalid class on our email field
+window.addEventListener("load", () => {
+    // Here, we test if the field is empty (remember, the field is not required)
+    // If it is not, we check if its content is a well-formed email address.
+    const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+    email.className = isValid ? "valid" : "invalid";
+});
+
+// This defines what happens when the user types in the field
+email.addEventListener("input", () => {
+    const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+    if (isValid) {
+        email.className = "valid";
+        error.textContent = "";
+        error.className = "error";
+    } else {
+        email.className = "invalid";
+    }
+});
+
+// This defines what happens when the user tries to submit the data
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+    if (!isValid) {
+        email.className = "invalid";
+        error.textContent = "I expect an email, darling!";
+        error.className = "error active";
+    } else {
+        email.className = "valid";
+        error.textContent = "";
+        error.className = "error";
+    }
+});
