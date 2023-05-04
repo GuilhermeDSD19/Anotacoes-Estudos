996181b6e7d31a03add665c9314280720d52d483
GuilhermeDSD19 <guilhermesilva19860@gmail.com>
2023-05-04 12:24:43 -0300

    Organizando o repositorio

---
+++ dev-quest/js/spread.js
@@ -0,0 +1,5 @@
+const pessoas = ['Guilherme', "Maria", "Alex", "Adriano",]
+const frutas = ["Banana", "Maça", "Uva",]
+
+console.log(pessoas, frutas) // [ 'Guilherme', 'Maria', 'Alex', 'Adriano' ] [ 'Banana', 'Maça', 'Uva' ]
+console.log(...pessoas, ...frutas) // Guilherme Maria Alex Adriano Banana Maça Uva
