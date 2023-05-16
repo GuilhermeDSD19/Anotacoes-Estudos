0f982bbeffd03b2a68dd9e260d79802508db56ea
GuilhermeDSD19 <guilhermesilva19860@gmail.com>
2023-05-16 19:49:09 -0300

    Metodos de Promises

---
+++ otavio-miranda/metodoPomises.js
@@ -0,0 +1,35 @@
+function rand(min, max) {
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if (typeof msg !== 'string') reject(false);
+
+        setTimeout(() => {
+            resolve(msg);
+        }, tempo);
+    });
+}
+
+// const promises = [
+//     'Primeiro valor',
+//     esperaAi('Promise 1', 3000),
+//     esperaAi('Promise 2', 500),
+//     esperaAi('Promise 3', 1000),
+//     "Outro valor"
+// ]
+
+const promises = [
+    esperaAi('Promise 1', rand(1, 5)),
+    esperaAi('Promise 2', rand(1, 5)),
+    esperaAi('Promise 3', rand(1, 5)),
+]
+
+Promise.race(promises)
+    .then(valor => {
+        console.log(valor)
+    })
+    .catch(erro => console.log(erro))
