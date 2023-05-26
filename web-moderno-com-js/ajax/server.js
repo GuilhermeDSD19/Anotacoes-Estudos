9b80fcb5b74cf484e65f6bd68363848167dc7fd5
GuilhermeDSD19 <guilhermesilva19860@gmail.com>
2023-05-26 19:33:59 -0300

    Upload de arquivo utilizando Ajax (XHR)

--- web-moderno-com-js/ajax/server.js
+++ web-moderno-com-js/ajax/server.js
@@ -6,4 +6,25 @@ app.use(express.static("."))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const multer = require("multer")
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post("/upload", (req, res) => {
+    upload(req, res, err => {
+        if (err) return res.end("Ocoreu um erro")
+        
+        res.end('Concluido com sucesso.')
+    })
+})
+
 app.listen(8080, () => console.log("Executando..."))
