d8d476117ca53f79d5acc5a8bfcb88ecff1f5189
GuilhermeDSD19 <guilhermesilva19860@gmail.com>
2023-06-05 15:24:01 -0300

    CommonJS X ESModules

---
+++ dev-quest/js/modulos-js/index.mjs
@@ -0,0 +1,4 @@
+import { somar, multiplicar } from "./operacoes-matematicas.mjs"
+
+console.log(somar(5, 10)) // 15
+console.log(multiplicar(5, 10)) // 50
---
+++ dev-quest/js/modulos-js/operacoes-matematicas.mjs
@@ -0,0 +1,9 @@
+function somar(n1, n2) {
+    return n1 + n2;
+}
+
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+
+export { somar, multiplicar };  
