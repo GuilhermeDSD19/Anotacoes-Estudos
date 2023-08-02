b9cc797a62297563a8f84098de25bdadd611c537
guilhermedsd19 <guilhermesilva19860@gmail.com>
2023-08-01 22:31:34 -0300

    Express Middlewares

--- otavio-miranda/node/aula03/server.js
+++ otavio-miranda/node/aula03/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const path = require("path");
+const { middlewareGlobal } = require("./src/middleware/middleware");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -10,6 +11,8 @@ app.use(express.static(path.resolve(__dirname, "public")));
 app.set('views', path.resolve(__dirname, "src", "views"))
 app.set('view engine', 'ejs')
 
+// Nossos Middlewares
+app.use(middlewareGlobal);
 app.use(routes)
 
 app.listen(3000, () => {
