f5c5de2466ef05625f65a1a2a7a26bd13954a4c4
GuilhermeDSD19 <guilhermesilva19860@gmail.com>
2024-05-19 21:19:21 -0300

    Challenge do Bootcamp Alura e Oracle

---
+++ ONE Next Education/portifolioAlura/javascript/toggleMenus.js
@@ -0,0 +1,17 @@
+let btn  = document.getElementById("btn-menu");
+let menu  = document.getElementById("sm-nav-links");
+
+
+
+function toggleMenu() {
+    if (menu.classList.contains("hidden-list")){
+        menu.classList.remove("hidden-list");
+        menu.classList.add("show-list");
+    } else {
+        menu.classList.remove("show-list");
+        menu.classList.add("hidden-list");
+    }
+}
+
+btn.onclick = toggleMenu;
+link.onclick = toggleMenu;
