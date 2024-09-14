e0d005076054401fec816869294dd86b781d18a7
GuilhermeDSD19 <guilhermesilva19860@gmail.com>
2024-09-14 18:49:05 -0300

    fix: Adicionado o Favicon e os links das redes sociais no HTML

--- dev-quest/dev-em-dobro/src/js/index.js
+++ dev-quest/dev-em-dobro/src/js/index.js
@@ -1,29 +1,29 @@
-const botoesCarrossel = document.querySelectorAll('.botao')
-const imagens = document.querySelectorAll('.imagem')
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
 
-function esconderImagemAtiva() {
-    const imagemAtiva = document.querySelector('.ativa')
-    imagemAtiva.classList.remove('ativa')
-}
-
-function removerBotaoSelecionado() {
-    const botaoSelecionado = document.querySelector('.selecionado')
-    botaoSelecionado.classList.remove('selecionado')
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
 }
 
 function selecionarBotaoCarrosel(botao) {
-    botao.classList.add('selecionado')
+    botao.classList.add('selecionado');
 }
 
-function mostrarImagemDeFundo(indice) {
-    imagens[indice].classList.add('ativa')
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
 }
 
 botoesCarrossel.forEach((botao, indice) => {
     botao.addEventListener('click', () => {
-        removerBotaoSelecionado()
-        selecionarBotaoCarrosel(botao)
-        removerImagemSelecionada()
-        mostrarImagemDeFundo(indice)
+        desativarBotaoSelecionado();
+        selecionarBotaoCarrosel(botao);
+        esconderImagemAtiva();
+        mostrarImagemDeFundo(indice);
     })
 })
